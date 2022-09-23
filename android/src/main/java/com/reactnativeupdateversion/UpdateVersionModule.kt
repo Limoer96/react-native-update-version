574e7f3a01040ad7373514512d2e05b232383b59
limoer <myt19960503@outlook.com>
2022-09-23 16:43:45 +0800

    feat: init

--- android/src/main/java/com/reactnativeupdateversion/UpdateVersionModule.kt
+++ android/src/main/java/com/reactnativeupdateversion/UpdateVersionModule.kt
@@ -1,20 +1,71 @@
 package com.reactnativeupdateversion
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
+import android.util.Log
+import com.azhon.appupdate.listener.OnDownloadListenerAdapter
+import com.azhon.appupdate.manager.DownloadManager
+import com.facebook.react.bridge.*
+import com.facebook.react.modules.core.DeviceEventManagerModule
+import java.io.File
+import kotlin.math.roundToInt
 
 class UpdateVersionModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
     override fun getName(): String {
         return "UpdateVersion"
     }
-
-    // Example method
-    // See https://reactnative.dev/docs/native-modules-android
+    private var manager: DownloadManager? = null
+    private var lastPercent: Int = 0
     @ReactMethod
-    fun multiply(a: Int, b: Int, promise: Promise) {
-          promise.resolve(a * b)
+    fun update(config: ReadableMap) {
+      val url = if(config.hasKey("url")) config.getString("url") else ""
+      val apkName = if(config.hasKey("apkName")) config.getString("apkName") else "update.apk"
+      manager = DownloadManager.Builder(currentActivity!!).run {
+        if (url.isNullOrEmpty() || apkName.isNullOrEmpty()) {
+          return
         }
+        apkUrl(url)
+        apkName(apkName)
+        smallIcon(R.mipmap.ic_launcher)
+        onDownloadListener(listenerAdapter)
+        build()
+      }
+      manager!!.download()
+    }
+    @ReactMethod
+    fun cancel() {
+      if (manager != null) {
+        manager!!.cancel()
+      }
+    }
+    private fun sendEvent(ctx: ReactContext, eventName: String, data: WritableMap?) {
+      ctx.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(eventName, data)
+    }
+    private val listenerAdapter: OnDownloadListenerAdapter = object : OnDownloadListenerAdapter() {
+      override fun downloading(max: Int, progress: Int) {
+        super.downloading(max, progress)
+        val currentPercent = progress.div(max.toDouble()).times(100).roundToInt()
+        val data: WritableMap = Arguments.createMap()
+        data.putInt("percent", currentPercent)
+        data.putInt("progress", progress)
+        data.putInt("max", max)
+        if (currentPercent != lastPercent) {
+          sendEvent(reactContext, "downloadProgress", data)
+        }
+        lastPercent = currentPercent
+      }
 
+      override fun done(apk: File) {
+        super.done(apk)
+        val data = Arguments.createMap()
+        data.putInt("percent", 100)
+        data.putString("apkPath", apk.path)
+        sendEvent(reactContext, "downloadProgress", data)
+      }
+
+      override fun error(e: Throwable) {
+        super.error(e)
+        val data = Arguments.createMap()
+        data.putString("message", e.message)
+        sendEvent(reactContext, "error", data)
+      }
+    }
     }
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { NativeModules, Platform } from 'react-native';
+import { NativeEventEmitter, NativeModules, Platform } from 'react-native';
 
 const LINKING_ERROR =
   `The package 'react-native-update-version' doesn't seem to be linked. Make sure: \n\n` +
@@ -6,7 +6,50 @@ const LINKING_ERROR =
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
 
-const UpdateVersion = NativeModules.UpdateVersion  ? NativeModules.UpdateVersion  : new Proxy(
+interface UpdateConfig {
+  url: string;
+  apkName?: string;
+}
+
+interface ProgressPayload {
+  /**
+   * 上传进度0-100整数
+   */
+  percent: number;
+  max?: number;
+  progress?: number;
+  /**
+   * 下载完成后的路径
+   */
+  apkPath?: string;
+}
+interface ErrorPayload {
+  /**
+   * 错误信息
+   */
+  message?: string;
+}
+interface UpdateVersionType {
+  /**
+   * 启动应用更新（下载apk并安装）
+   */
+  update: (config: UpdateConfig) => void;
+  /**
+   * 取消应用更新
+   */
+  cancel: () => void;
+  /**
+   * 更新过程中事件监听
+   */
+  listen: (
+    onProgress: (payload: ProgressPayload) => void,
+    onError?: (payload: ErrorPayload) => void
+  ) => () => void;
+}
+
+const UpdateVersion: UpdateVersionType = NativeModules.UpdateVersion
+  ? NativeModules.UpdateVersion
+  : new Proxy(
       {},
       {
         get() {
@@ -15,6 +58,21 @@ const UpdateVersion = NativeModules.UpdateVersion  ? NativeModules.UpdateVersion
       }
     );
 
-export function multiply(a: number, b: number): Promise<number> {
-  return UpdateVersion.multiply(a, b);
-}
+UpdateVersion.listen = (onProgress, onError) => {
+  const eventEmitter = new NativeEventEmitter(NativeModules.UpdateVersion);
+  const progressChangeEvent = eventEmitter.addListener(
+    'downloadProgress',
+    (payload) => {
+      onProgress(payload);
+    }
+  );
+  const errorEvent = eventEmitter.addListener('error', (payload) => {
+    onError?.(payload);
+  });
+  return () => {
+    progressChangeEvent.remove();
+    errorEvent.remove();
+  };
+};
+
+export default UpdateVersion;
